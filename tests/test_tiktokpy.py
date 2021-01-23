b8095431a13ec579a66a20cf240c2fbd9400a03e
Evgeny Kemerov <eskemerov@gmail.com>
2021-01-23 20:38:50 +0300

    🔖Bump version: 0.8.8 → 0.8.9

--- tests/test_tiktokpy.py
+++ tests/test_tiktokpy.py
@@ -2,4 +2,4 @@ from tiktokpy import __version__
 
 
 def test_version():
-    assert __version__ == "0.8.8"
+    assert __version__ == "0.8.9"
--- tiktokpy/__init__.py
+++ tiktokpy/__init__.py
@@ -1,4 +1,4 @@
 from .bot import TikTokPy
 
-__version__ = "0.8.8"
+__version__ = "0.8.9"
 __all__ = ["TikTokPy"]

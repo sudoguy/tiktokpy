5f7b8c7a5348eadb3dd7fc71a7a8d41cf7458ce2
ZakariabenSalem <zakariabensalem3@gmail.com>
2023-03-22 16:07:07 +0100

    Sign up with twitter

--- quicklogin.py
+++ quicklogin.py
@@ -5,8 +5,10 @@ from tiktokpy import TikTokPy
 
 async def main():
     async with TikTokPy() as bot:
-        # Login to TikTok
-        await bot.login_session()
-
-
+        # create Twitter account
+        await bot.create_twitter()
+        # Sign up with created twitter account
+        await bot.login_with_twitter()
+        #
+#captcha_verify_img--wrapper sc-gZMcBi jzVByM
 asyncio.run(main())
